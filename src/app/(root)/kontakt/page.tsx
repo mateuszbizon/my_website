cd93c6de2d838fe1a5e7f340147b8b33534ccc96
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-08-01 10:10:43 +0200

    added contact page

---
+++ src/app/(root)/kontakt/page.tsx
@@ -0,0 +1,14 @@
+import Contact from '@/components/sections/Contact'
+import SubPageHero from '@/components/sections/SubPageHero'
+import React from 'react'
+
+function ContactPage() {
+  return (
+    <>
+        <SubPageHero title='Kontakt' />
+        <Contact />
+    </>
+  )
+}
+
+export default ContactPage
