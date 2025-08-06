9a74721101bb0aa08e66edefe2917d9e5993b64c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-08-06 19:16:04 +0200

    changed single post links

--- src/app/(root)/[slug]/page.tsx
+++ src/app/(root)/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import SubPageHero from '@/components/sections/SubPageHero'
 import { Button } from '@/components/ui/button'
 import Container from '@/components/ui/container'
 import { cn } from '@/lib/utils'
@@ -24,18 +25,7 @@ async function PostPage({ params }: Props) {
 
   return (
     <>
-        <header className='relative py-section-padding lg:py-[calc(theme(padding.section-padding)*2)] z-0'>
-            <Container>
-                <h1 className='heading1 text-center'>{post.title}</h1>
-            </Container>
-            <div
-                className={cn(
-                "absolute inset-0 opacity-70 -z-10",
-                "[background-size:40px_40px]",
-                "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                )}
-            />
-        </header>
+        <SubPageHero title={post.title || ""} />
         <section className='pb-section-padding'>
             <Container>
                 {post.mainImage && (
@@ -53,10 +43,12 @@ async function PostPage({ params }: Props) {
                     <div className='flex flex-col gap-5 justify-center md:w-1/3 -order-1 md:order-2 bg-foreground text-background-light p-7 rounded-2xl'>
                         <div className='space-y-1'>
                             <p className='text-xl font-semibold'>Adres strony</p>
-                            <p className='text-lg font-medium'>
-                                <Link href={post.websiteLink || "#"} target='_blank'>
-                                    {post.websiteName}
-                                </Link>
+                            <p>
+                                <Button variant={"link"} size={"link"} className='text-background-light hover:text-background-light/70 text-lg' asChild>
+                                    <Link href={post.websiteLink || "#"} target='_blank'>
+                                        {post.websiteName}
+                                    </Link>
+                                </Button>
                             </p>
                         </div>
                         <div className='space-y-1'>
@@ -83,12 +75,12 @@ async function PostPage({ params }: Props) {
                 </p>
                 <div className='flex justify-center gap-5 flex-wrap'>
                     <Button className='md:text-lg' size={"lg"} asChild>
-                        <Link href={"/"}>
+                        <Link href={"/darmowa-wycena"}>
                             Zapytaj o ofertę
                         </Link>
                     </Button>
                     <Button className='md:text-lg' size={"lg"} variant={"outline"} asChild>
-                        <Link href={"/"}>
+                        <Link href={"/portfolio"}>
                             Portfolio
                         </Link>
                     </Button>
