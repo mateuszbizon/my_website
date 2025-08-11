2269c839e8a9f0e03729e67fd9fc79ea4d99ec61
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-08-11 09:55:35 +0200

    added image to language school page

--- src/app/(root)/oferta/strona-szkol-jezykowych/page.tsx
+++ src/app/(root)/oferta/strona-szkol-jezykowych/page.tsx
@@ -3,6 +3,7 @@ import OfferAbout from '@/components/sections/OfferAbout'
 import OfferPluses from '@/components/sections/OfferPluses'
 import Questions from '@/components/sections/Questions'
 import SubPageHero from '@/components/sections/SubPageHero'
+import { LANGUAGE_SCHOOL_IMAGE } from '@/constants/images'
 import { LANGUAGE_SCHOOL_OFFER, WEB_SITE_PLUSES } from '@/constants/offerInfo'
 import React from 'react'
 
@@ -10,7 +11,7 @@ function LanguageSchoolPage() {
   return (
     <>
         <SubPageHero title='Strona internetowa dla szkół językowych' />
-        <OfferAbout title={LANGUAGE_SCHOOL_OFFER.title} description={LANGUAGE_SCHOOL_OFFER.description} features={LANGUAGE_SCHOOL_OFFER.list} />
+        <OfferAbout title={LANGUAGE_SCHOOL_OFFER.title} description={LANGUAGE_SCHOOL_OFFER.description} features={LANGUAGE_SCHOOL_OFFER.list} image={LANGUAGE_SCHOOL_IMAGE} />
         <OfferPluses plusesList={WEB_SITE_PLUSES} />
         <HowProjectWork />
         <Questions />
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -9,3 +9,8 @@ export const BARBER_IMAGE: Image = {
     src: "/barber_img.png",
     alt: "Ikona przedstawiająca fryzjera z narzędziami do strzyżenia z klientem.",
 }
+
+export const LANGUAGE_SCHOOL_IMAGE: Image = {
+    src: "/language_school_img.png",
+    alt: "Ikona przedstawiająca nauczyciela i ucznia stojących przed tablicą.",
+}
