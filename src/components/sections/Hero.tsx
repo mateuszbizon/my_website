b0e3d6419bdafdb34a66a4b3ec591c5525125243
Mateusz Bizoń <mat-biz@wp.pl>
2025-08-18 11:00:49 +0200

    changed hero button text

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -57,7 +57,7 @@ function Hero() {
                     <div className='flex justify-center gap-5 flex-wrap'>
                         <Button className='button' size={"lg"}>
                             <Link href={"/portfolio"}>
-                                Zobacz portfolio
+                                Zobacz
                             </Link>
                         </Button>
                         <Button className='button' size={"lg"} variant={"outline"}>
