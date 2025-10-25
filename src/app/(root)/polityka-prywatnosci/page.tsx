cf961943e94cf73a5d28b1b55fec043b98d0f58f
Mateusz Bizoń <mat-biz@wp.pl>
2025-10-25 10:43:43 +0200

    changed policy text

--- src/app/(root)/polityka-prywatnosci/page.tsx
+++ src/app/(root)/polityka-prywatnosci/page.tsx
@@ -56,7 +56,7 @@ function PrivatePolicyPage() {
                     <div className='space-y-3'>
                         <h2 className='bigger-text'>4. Podstawa prawna przetwarzania danych</h2>
                         <p>
-                            Dane są przetwarzane zgodnie z rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27.04.2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (dalej: RODO)
+                            Dane są przetwarzane zgodnie z rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27.04.2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE.
                         </p>
                     </div>
                     <div className='space-y-3'>
