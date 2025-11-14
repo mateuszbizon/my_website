f989f1308353233a81aa80db4a3ed621d97b0ec5
Mateusz Bizoń <mat-biz@wp.pl>
2025-11-14 14:54:46 +0100

    added react email templates

--- src/components/emails/InfoEmail.tsx
+++ src/components/emails/InfoEmail.tsx
@@ -1,3 +1,4 @@
+import { Body, Button, Head, Heading, Html, Section, Tailwind, Text } from "@react-email/components";
 import React from 'react'
 
 type InfoEmailProps = {
@@ -6,15 +7,20 @@ type InfoEmailProps = {
 
 function InfoEmail({ name }: InfoEmailProps) {
   return (
-    <div className='space-y-5'>
-        <h1>Nowy klient!</h1>
-        <p>Osoba o imieniu {name} coś od Ciebie chce ;)</p>
-        <p>
-            <a href={"https://airtable.com/apppRBMAXdruP7umw/tblrSBr8kd9OrXxqV/viw9rmu5yAfWejur3?blocks=hide"}>
-                Link do Airtable
-            </a>
-        </p>
-    </div>
+    <Html>
+        <Head />
+        <Tailwind>
+        <Body className="text-[#474554]">
+            <Section className='space-y-5'>
+                <Heading className="text-3xl font-bold">Nowy klient!</Heading>
+                <Text className="text-xl font-medium">Osoba o imieniu {name} coś od Ciebie chce ;)</Text>
+                <Button href={"https://airtable.com/apppRBMAXdruP7umw/tblrSBr8kd9OrXxqV/viw9rmu5yAfWejur3?blocks=hide"} className="bg-[#7E78D2] text-[#FBF7FF] rounded-xl text-base px-6 py-3">
+                    Link do Airtable
+                </Button>
+            </Section>
+        </Body>
+        </Tailwind>
+    </Html>
   )
 }
 
--- src/components/emails/NewFreePriceEmail.tsx
+++ src/components/emails/NewFreePriceEmail.tsx
@@ -1,3 +1,4 @@
+import { Body, Button, Head, Heading, Html, Section, Tailwind, Text } from "@react-email/components";
 import React from 'react'
 
 type NewFreePriceEmailProps = {
@@ -6,15 +7,20 @@ type NewFreePriceEmailProps = {
 
 function NewFreePriceEmail({ name }: NewFreePriceEmailProps) {
   return (
-    <div className='space-y-5'>
-        <h1>Nowa wycena!</h1>
-        <p>Osoba o imieniu {name} prosi Cię o wycenę ;)</p>
-        <p>
-            <a href={"https://airtable.com/apppRBMAXdruP7umw/tblaeV7Vsa5FWlN9q/viwfHat2q7nncHZOn?blocks=hide"}>
-                Link do Airtable
-            </a>
-        </p>
-    </div>
+    <Html>
+        <Head />
+        <Tailwind>
+        <Body className="text-[#474554]">
+            <Section className='space-y-5'>
+                <Heading className="text-3xl font-bold">Nowa wycena!</Heading>
+                <Text className="text-xl font-medium">Osoba o imieniu {name} prosi Cię o wycenę ;)</Text>
+                <Button href={"https://airtable.com/apppRBMAXdruP7umw/tblaeV7Vsa5FWlN9q/viwfHat2q7nncHZOn?blocks=hide"} className="bg-[#7E78D2] text-[#FBF7FF] rounded-xl text-base px-6 py-3">
+                    Link do Airtable
+                </Button>
+            </Section>
+        </Body>
+        </Tailwind>
+    </Html>
   )
 }
 
